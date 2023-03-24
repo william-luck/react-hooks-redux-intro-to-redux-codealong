defc9e1eee192a62f541173ae929594300a339cb
William Luck <luck.william.h@gmail.com>
2023-03-24 11:29:35 -0300

    commit tree 2cf2fc9c5cab5f5498902ed0d68486a8ca23dd60

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,8 +1,11 @@
 import React from "react";
+// Two necessary imports for changing and accessing state
 import { useDispatch, useSelector } from "react-redux";
 
 function Counter() {
   // read from the Redux store
+  // Takes a callback function, whatever the callback function returns is assigned to items.
+  // Also responsible for re-rendering, causing the componnent to re-render whenever state.items changes 
   const items = useSelector((state) => state.items);
 
   // gives us the dispatch function to send actions to the Redux store
--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@ import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Additions
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// The reducer is defined in a separate features file
+const store = createStore(
+    counterReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
+// Use the Provider to wrap app, passing store down as a prop. 
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
+
